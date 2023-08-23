ead800c3ec60f56f1a9abb346cd2ca757295371d
jimbo <jimbo@91932.com>
2023-08-23 10:46:32 +0900

    問５

--- app/Http/Controllers/DrinksController.php
+++ app/Http/Controllers/DrinksController.php
@@ -5,26 +5,32 @@ use illuminate\Http\Request;
 class DrinksController extends Controller
 {
     public function index(Request $request)
-{
+    {
 
-    if ($request->hasCookie("accessCount")) {
-        $count = $request->cookie("accessCount");
-    } else {
-        $count = 0;
+        if ($request->hasCookie("accessCount")) {
+            $count = $request->cookie("accessCount");
+        } else {
+            $count = 0;
+        }
+
+        $count ++;
+
+        $drinks = $this->getDrinks();
+
+        // returnの記述を以下のように修正する
+        return response()
+            ->view("drinks.index", [
+                "drinks" => $drinks,
+                "accessCount" => $count
+            ])
+            ->cookie("accessCount", $count);
     }
-
-    $count ++;
-
-    $drinks = $this->getDrinks();
-
-    // returnの記述を以下のように修正する
-    return response()
-        ->view("drinks.index", [
-            "drinks" => $drinks,
-            "accessCount" => $count
-        ])
-        ->cookie("accessCount", $count);
-}
+    public function cookieDelete(){
+        $forget = \Cookie::forget("accessCount");
+        return response()
+      ->view("drinks.cookieDelete")
+      ->cookie($forget);
+    }
 
    public function getDrinks(){
     $drinks = [
