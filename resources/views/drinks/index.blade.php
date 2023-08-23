7702e476eb45a45edb541818f49a04ef99056b09
jimbo <jimbo@91932.com>
2023-08-24 02:24:48 +0900

    index.bladeの記述統一

--- resources/views/drinks/index.blade.php
+++ resources/views/drinks/index.blade.php
@@ -5,8 +5,6 @@
     <meta charset="utf8" />
 </head>
 <body>
-    <div>
-    </div>
     <h1>飲み物一覧</h1>
     <table>
         <thead>
@@ -14,26 +12,57 @@
             <th>価格</th>
             <th>在庫数</th>
             <th>メーカー名</th>
+            <th></th>
+            <th></th>
         </thead>
         <tbody>
             <?php foreach($drinks as $drink): ?>
                 <tr>
-                    <td><?php echo $drink->name; ?></td>
-                    <td><?php echo $drink->price; ?></td>
-                    <td><?php echo $drink->stock; ?></td>
-                    <td><?php echo $drink->maker->name; ?></td>
-                    <td><a href="drinks/edit/{{$drink->id}}">編集</a></td>
-                    <td><form action="<?php echo url('drinks/delete/'. $drink->id); ?>" method="POST">
-                        @csrf
-                        <div><input type="submit" value="削除"></div>
-                    </form></td>
+                    <td>{{$drink->name}}</td>
+                    <td>{{$drink->price}}</td>
+                    <td>{{$drink->stock}}</td>
+                    <td>{{$drink->maker->name}}</td>
+                    <td><a href="{{url('drinks/edit/'. $drink->id)}}">編集</a></td>
+                    <td>
+                        <form action="{{url('drinks/delete/'. $drink->id)}}" method="POST">
+                            @csrf
+                            <div>
+                                <input type="submit" value="削除">
+                            </div>
+                        </form>
+                    </td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
     </table>
-
-
-
+    <hr>
+    <form action="{{url('drinks')}}" method="GET">
+        <div>
+            <label for="name">飲み物名</label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div>
+            <label for="price">価格</label>
+            <select name="price" id="price">
+                <option value="1" selected>指定なし</option>
+                <option value="2">100円未満</option>
+                <option value="3">100~150円未満</option>
+                <option value="4">150円以上</option>
+            </select>
+        </div>
+        <div>
+        <label for="stock">在庫数</label>
+            <select name="stock" id="stock">
+                <option value="1" selected>指定なし</option>
+                <option value="2">30個以下</option>
+                <option value="3">31~50個以下</option>
+                <option value="4">51~100個以下</option>
+                <option value="5">101個以上</option>
+            </select>
+        </div>
+        <div>
+            <input type="submit" value="検索">
+        </div>
+    </form>
 </body>
 </html>
-
